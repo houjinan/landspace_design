8c6f56e48e56b53bae2b0a4248b9f75f0af356de
houjinan@126.com <houjinan@126.com>
2016-10-24 17:39:30 +0800

    modified style

--- app/models/service_case.rb
+++ app/models/service_case.rb
@@ -2,5 +2,6 @@ class ServiceCase < ApplicationRecord
   has_many :attachments, :as => :resource, :dependent => :destroy
   accepts_nested_attributes_for :attachments, allow_destroy: true
 
+  ServiceCategory = {商业空间: "commercial_space", 居住空间: "living_space", 办公空间: "office_space"}
   validates :title_zh, :title_en, presence: {message: "标题不能为空"}
 end
