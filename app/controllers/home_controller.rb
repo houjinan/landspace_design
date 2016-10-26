a4c71495f16b664a7544eb5d748020e66c28d7a8
houjinan@126.com <houjinan@126.com>
2016-10-26 10:46:12 +0800

    modified site member

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
 
   def site
     @service_cases = ServiceCase.all.limit(6)
+    @members = Member.all.limit(6)
     render "site", layout: "banner_index"
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -5,4 +5,22 @@ class Member < ApplicationRecord
   validates :name_zh, :name_en, presence: {message: "名称不能为空"}
   validates :head_avatar, presence: {message: "请上传本人照片"}
 
+
+  def name
+    case I18n.locale.to_s.downcase
+    when "en"
+      self.name_en
+    else
+      self.name_zh
+    end
+  end
+
+  def description
+    case I18n.locale.to_s.downcase
+    when "en"
+      self.description_en
+    else
+      self.description_zh
+    end
+  end
 end
